3fce398cfef5ebb4cb580562508fdbc64ff32cfa
taoketao <morgan.r.bryant@gmail.com>
2022-07-11 16:54:17 -0700

    quick-synced :wq Mon Jul 11 16:54:00 2022 Very first verified result is in. Indeed it's perfectly accurate!

--- signal-follows-D2-check-experiments/notebook_launcher.sh
+++ signal-follows-D2-check-experiments/notebook_launcher.sh
@@ -1,5 +1,6 @@
-
-
+echo ''
+echo '**** Navigate to localhost:8000; ignore jupyter ****'
+echo ''
 docker run --rm \
     -v $(pwd)/tf/:/tf/ \
     --privileged \
@@ -8,10 +9,3 @@ docker run --rm \
     --workdir /tf/ \
     -it \
     tensorflow/tensorflow:latest-jupyter 
-#    bash ./.run_jupyter.sh
-
-    
-  # jupyter/tensorflow-notebook:latest
-#    bash
-#    jupyter notebook  --autoreload --allow-root
-#    jupyter notebook --port=8061 --autoreload --allow-root
